53086a1fc84eb3d6bd134b36d0496911bcf93e9a
Stefan Moraru <stefan@stefanmoraru.ro>
2017-05-06 12:48:08 +0300

    Improvements

--- api/index.js
+++ api/index.js
@@ -2,43 +2,13 @@ const express = require('express');
 const Please = require('../src/services/_.js');
 const app = express();
 const PORT = 5000;
+const API_URL = 'http://api.please.com:5000';
+
 let settings = {
   pluginMatchProbabilityThreshold: 0.3,
   user: null,
   plugins: null
 };
-
-app.get('/api/v1/conversation/', (req, res) => {
-	res.status(400).send(`Input text must not be empty`);
-});
-
-app.get('/api/v1/conversation/:text', (req, res) => {
-	const text = req.params.text;
-
-  let plugin = Please.bestPluginMatch(settings, {
-		text: text
-	});
-
-	let currentStep = Please._currentStep(plugin);
-
-	if (!plugin || (plugin && !plugin.name)) {
-		//TODO: Create function
-		res.status(404).send(`Could not find plugin for input ${text} `)
-	}
-
-  if (currentStep.query && !currentStep.queryDone) {
-		Please._query(currentStep, plugin.params)
-		.then((data) => {
-			res.json(data);
-		})
-		.catch((error) => {
-			res.status(503).send(`Could not send query ${error}`);
-		})
-
-	} else {
-	  res.json(currentStep);
-	}
-})
 
 const init = () => {
 	Please.fb_onValue('plugins', (plugins) => {
@@ -47,6 +17,47 @@ const init = () => {
 	});
 };
 
-app.listen(PORT, init);
+const renderPluginStep = (req, res) => {
+	const text = req.params.text;
+  const step = req.params.step;
+  const plugin = Please.bestPluginMatch(settings, {
+		text: text
+	});
 
-//TODO: DecodeURI from text input, to leave out %20
+	if (step) {
+		plugin.step = step;
+	}
+
+	let currentStep = Please._currentStep(plugin);
+
+	if (Please._emptyPlugin(plugin)) {
+		return res.status(404).send(`Could not find plugin for input ${text} `)
+	}
+
+  if (currentStep && currentStep.query && !currentStep.queryDone) {
+		promise = Please._query(plugin, currentStep);
+	} else {
+		promise = new Promise((resolve, reject) => { resolve(); });
+	}
+
+	return promise.then(() => {
+	  return res.json(Please._formatStep(currentStep, `${API_URL}${req.path}`));
+	})
+	.catch((error) => {
+		return res.status(503).send(error);
+	});
+}
+
+app.get('/api/v1/conversation/', (req, res) => {
+	return res.status(400).send(`Input text must not be empty`);
+});
+
+app.get('/api/v1/conversation/:text/:step', (req, res) => {
+	return renderPluginStep(req, res);
+});
+
+app.get('/api/v1/conversation/:text', (req, res) => {
+	return renderPluginStep(req, res);
+});
+
+app.listen(PORT, init);
